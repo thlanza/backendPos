d6921716b71db130318373c70986cc3e44929e17
Thiago Lanza <thlanza@hotmail.com>
2023-01-05 12:08:30 -0300

    agora com autenticação do google

--- alunoRoutes/alunos/alunoRoutes.js
+++ alunoRoutes/alunos/alunoRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { matricular, seedAlunos, getAlunos, deletarColecaoAlunos } = require('../../alunoControllers/alunos/alunoController');
 const authMiddleware = require('../../middlewares/auth/authMiddeware');
 const { photoUpload, redimensionar } = require('../../middlewares/upload/photoUpload');
+const passport = require("passport");
 
 const alunoRouter = express.Router();
 
@@ -15,5 +16,41 @@ alunoRouter.post('/matricular',
 alunoRouter.post('/seedAlunos', seedAlunos);
 alunoRouter.delete('/deletarColecaoAlunos', deletarColecaoAlunos);
 
+//google login
+alunoRouter.get("/login/success", (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            error: false,
+            message: "Successfully logged in",
+            user: req.user
+        })
+    } else {
+        res.status(403).json({ error: true, message: "Not Authorized" })
+    }
+});
+
+
+alunoRouter.get("/login/failed", (req, res) => {
+    res.status(401).json({
+        error: true,
+        message: "Login failure"
+    })
+});
+
+
+alunoRouter.get(
+    "/google/callback",
+    passport.authenticate("google", { 
+        successRedirect: process.env.CLIENT_URL,
+        failureRedirect: "/login/failed" 
+    }),
+);
+
+alunoRouter.get("/google", passport.authenticate("google", ["profile", "email"]));
+
+alunoRouter.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect(process.env.CLIENT_URL);
+});
 
 module.exports = alunoRouter;
--- server.js
+++ server.js
@@ -7,15 +7,34 @@ const adminRouter = require('./adminRoutes/admins/adminRoute');
 const modalidadeRouter = require('./adminRoutes/modalidadeRoutes.js/modalidadeRoute');
 const alunoRouter = require('./alunoRoutes/alunos/alunoRoutes');
 const app = express();
+const passport = require('passport');
+const passportSetup = require('./passport');
+const cookieSession = require('cookie-session');
 
 //DB
 dbConnect();
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
+app.use(
+    cookieSession({
+        name: "session",
+        keys: ["thlanza"],
+        maxAge: 24 * 60 * 60 * 100
+    })
+);
+
 app.use(express.urlencoded({ extended: true }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/admin', adminRouter);
 app.use('/api/modalidades', modalidadeRouter);
