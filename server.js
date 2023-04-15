01ea43913164be47f1e403e23decc621a8d25b3b
Thiago Lanza <thlanza@hotmail.com>
2023-04-15 17:43:40 -0300

    google strategy scope

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ passport.use(new GoogleStrategy({
     clientID:     process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: `${process.env.CLIENT_URL}/auth/google/callback`,
-    passReqToCallback   : true
+    passReqToCallback   : true,
+    scope: ['profile', 'email']
   }, authUser));
 
 
